845c4c6a3e2b0be1e6dca9a50e4e965a4a5392b1
codemasterfunk <148785150+codemasterfunk@users.noreply.github.com>
2024-10-09 09:02:03 -0400

    Initial commit

---
+++ snake.js
@@ -0,0 +1,77 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/AaGK-fj-BAM
+
+function Snake() {
+  this.x = 0;
+  this.y = 0;
+  this.xspeed = 1;
+  this.yspeed = 0;
+  this.total = 0;
+  this.tail = [];
+  this.color = [random(255), random(255), random(255)];
+  this.colorDelta = 255;
+
+  this.eat = function(pos) {
+    var d = dist(this.x, this.y, pos.x, pos.y);
+    if (d < 1) {
+      this.total++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  this.dir = function(x, y) {
+    this.xspeed = x;
+    this.yspeed = y;
+  }
+
+  this.death = function() {
+    for (var i = 0; i < this.tail.length; i++) {
+      var pos = this.tail[i];
+      var d = dist(this.x, this.y, pos.x, pos.y);
+      if (d < 1) {
+        console.log('starting over');
+        this.total = 0;
+        this.tail = [];
+      }
+    }
+  }
+
+  this.update = function() {
+    for (var i = 0; i < this.tail.length - 1; i++) {
+      this.tail[i] = this.tail[i + 1];
+    }
+    if (this.total >= 1) {
+      this.tail[this.total - 1] = createVector(this.x, this.y);
+    }
+
+    this.x = this.x + this.xspeed * scl;
+    this.y = this.y + this.yspeed * scl;
+
+  
+    this.x = constrain(this.x, 0, width - scl);
+    this.y = constrain(this.y, 0, height - scl);
+    let xColor = map(this.x, 0, width, 0, 360);
+    let yColor = map(this.y, 0, height, 255, 0);
+    this.color = [xColor, yColor, 255];
+//     for (let i = 0; i < this.color.length; ++i) {
+//       this.color[i] = random(this.color[i] - this.colorDelta, 
+//                              this.color[i] + this.colorDelta);
+//     }
+    
+  }
+
+  this.show = function() {
+    push();
+    colorMode(HSB);
+    fill(this.color);
+    for (var i = 0; i < this.tail.length; i++) {
+      rect(this.tail[i].x, this.tail[i].y, scl, scl);
+    }
+    rect(this.x, this.y, scl, scl);
+    pop();
+  }
+}
